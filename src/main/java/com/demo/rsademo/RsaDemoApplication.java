19692f1e410a4b7706ac9c93ec66823b50ea5075
TonyZeng <605975553@qq.com>
2018-12-13 23:03:20 +0800

    add some config files

---
+++ src/main/java/com/demo/rsademo/RsaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.rsademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsaDemoApplication.class, args);
+	}
+}
